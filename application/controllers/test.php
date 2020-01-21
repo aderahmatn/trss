b331e801c6584c33e6cf4bcd0bdb8aa5b0f44437
ade rahmat nurdiyana <nurdiyana.ade@gmail.com>
2020-01-21 22:58:00 +0700

    asdd
    asd

--- application/controllers/test.php
+++ application/controllers/test.php
@@ -1,1 +1,14 @@
-test.php
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Test extends CI_Controller {
+
+	public function index()
+	{
+		
+	}
+
+}
+
+/* End of file test.php */
+/* Location: ./application/controllers/test.php */
